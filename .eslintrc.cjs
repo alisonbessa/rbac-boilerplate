a27e75efd0ed8b45a7c0ab8a6d64f4d8eee674e5
alisonbessa <alisonbessa@gmail.com>
2025-08-11 15:55:25 -0300

    chore(lint): unify ESLint/Prettier config via shared package and fix lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,6 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  ignorePatterns: ['dist', 'build', 'node_modules'],
-  env: { es2022: true, node: true, browser: true },
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
+  extends: [require.resolve('./packages/config/eslint/index.cjs')],
 };
 
 
